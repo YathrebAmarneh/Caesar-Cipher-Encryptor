1830c64d14621a01071c11e7b547f838a9eb55b0
Yathreb <yathrebamarneh93@gmail.com>
2021-04-06 21:22:18 +0300

    update

--- encryptor.js
+++ encryptor.js
@@ -2,28 +2,25 @@
 //program to implement Caesar Cipher Encryptor
 
 function encryptor(str) {
-    const k = 2; // key encryptor
-    const englishAlphabet = "abcdefghijklmnopqrstuvwxyzab".split("")
+    const k = 7; // key encryptor
+    const englishAlphabet = "abcdefghijklmnopqrstuvwxyz".split("") //26 char
     strArr = str.split("")
     // console.log(englishAlphabet)
     // console.log(strArr)
 
     for (let i = 0; i < strArr.length; i++) {
-        for (let j = 0; j < englishAlphabet.length - k; j++) {
-            if (strArr[i] === englishAlphabet[j]) {
-
-                strArr[i] = englishAlphabet[j + k]
-                // console.log(strArr[i])
-                break;
-
-            }
+        j = englishAlphabet.indexOf(strArr[i])
+        if (j >= englishAlphabet.length - k) {
+            f = (j + k) % englishAlphabet.length
+            strArr[i] = englishAlphabet[f]
         }
-
-    }
+        else {
+            strArr[i] = englishAlphabet[j + k]
+        }
+    } //end of for loop
 
     console.log(strArr)
 }
-
-str = "xyz" // output ="zab"
+str = "mvklahvjcnbwqvtutmfafkwiuagjkzmzwgf"
 
 encryptor(str)
